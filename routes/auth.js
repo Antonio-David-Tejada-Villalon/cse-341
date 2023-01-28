fc6a0fb37d4349afd99571fca8c329b2a8215068
Antonio-David-Tejada-Villalon <4vdel777@gmail.com>
2023-01-28 01:16:37 -0300

    Lesson07-main

---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const dotenv = require('dotenv');
+dotenv.config();
+const { auth, requiresAuth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+router.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+router.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+router.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+})
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 // const book = require('../controllers/books');
 
 router.use('/', require('./swagger'));
+router.use('/', require('./auth'));
 //requiring books
 router.use('/books', require('../routes/books'));
 
