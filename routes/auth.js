75c56a807c6dc243320075c04c70a99096c320e4
Antonio-David-Tejada-Villalon <4vdel777@gmail.com>
2023-01-28 22:56:34 -0300

    L07-render

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const { auth, requiresAuth } = require('express-openid-connect');
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: process.env.SECRET,
+  secret: 'GM0C-x7V_GVvAZVEZKG8HalKkJwbKS_ojqzOnVGC8ivYMDRDN028MDPoQtNAds4X',
   baseURL: process.env.BASE_URL,
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.ISSUER_BASE_URL
