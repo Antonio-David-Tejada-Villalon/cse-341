27e9a74b3d7adf94f2b8a027dd4db4ad3c462226
Antonio-David-Tejada-Villalon <4vdel777@gmail.com>
2023-02-05 23:37:17 -0300

    Lesson08-render1

--- Lesson08-render/routes/books.js
+++ Lesson08-render/routes/books.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const bookController = require('../controllers/books');
-const validation = require('../midware/validate')
+const validation = require('../midware/validate');
+const authLogin = require('../midware/authorize');
 
-router.get('/', bookController.allBooks);
-router.get('/:id', bookController.oneBook);
-router.post('/', validation.checkBook, bookController.createABook);
-router.put('/:id', validation.checkBook, bookController.updateBook);
-router.delete('/:id', bookController.deleteBook);
+router.get('/', authLogin.verifyLogin, bookController.allBooks);
+router.get('/:id', authLogin.verifyLogin,bookController.oneBook);
+router.post('/', authLogin.verifyLogin, validation.checkBook, bookController.createABook);
+router.put('/:id', authLogin.verifyLogin, validation.checkBook, bookController.updateBook);
+router.delete('/:id', authLogin.verifyLogin, bookController.deleteBook);
 
 
 module.exports = router;
--- Lesson08-render/routes/index.js
+++ Lesson08-render/routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const { requiresAuth } = require('express-openid-connect');
 const router = express.Router();
-// const book = require('../controllers/books');
+const authLogin = require('../midware/authorize');
 
 router.use('/', require('./swagger'));
-router.use('/', require('./auth'));
-//requiring books
-router.use('/books', require('../routes/books'));
 
-router.use('/', (req, res) => {
-    res.send('This Page is working')
+//requiring books
+router.use('/books', authLogin.verifyLogin, requiresAuth(), require('../routes/books'));
+
+router.use('/', authLogin.verifyLogin, requiresAuth(), (req, res) => {
+    // res.send(JSON.stringify(req.oidc.books));
+    res.send('This Page is working');
 })
 
 module.exports = router;
